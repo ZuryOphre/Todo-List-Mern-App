d7505b391af82a26b311be7e0b9032ea2ba0bae2
Zury Martinez <zury.martinez.saldivia@gmail.com>
2023-01-29 23:07:14 -0300

    fixed issue with render when adding and deleting items

--- client/todo-list-mernapp/src/App.js
+++ client/todo-list-mernapp/src/App.js
@@ -11,7 +11,7 @@ const addItem = async (e) => {
   e.preventDefault();
   try{
     const res = await axios.post('http://localhost:5500/api/item', {item: itemText})
-    console.log(res);
+    setListItems(prev => [...prev, res.data]);
     setItemText('');
   }catch(err){
     console.log(err);
@@ -24,13 +24,14 @@ useEffect(()=>{
       try{
         const res = await axios.get('http://localhost:5500/api/items')
         setListItems(res.data)
+        console.log('render')
       }catch(err){
         console.log(err);
       }
     }
     getItemsList()
 
-},[listItems])
+},[])
 
 //Delete item when click on delete
 const deleteItem = async (id) => {
@@ -55,7 +56,7 @@ const deleteItem = async (id) => {
         {
           listItems.map(item => (
             <div className="todo-item">
-              <p className="item-content">{item.item}1</p>
+              <p className="item-content">{item.item}</p>
               <button className="update-item">Update</button>
               <button className="delete-item" onClick={()=>{deleteItem(item._id)}}>Delete</button>
             </div>
